88074d92ee258847eb732ed8ec51e47b00683c5f
Matías Pavez <matias.pavez.b@gmail.com>
2017-01-12 14:00:46 -0300

    functions.sh as wrappers for python functions

--- shell/functions.sh
+++ shell/functions.sh
@@ -2,44 +2,60 @@
 
 _bender_admin_goodbye () 
 {
-    cat <<EOF
+    python "$BENDER_SYSTEM"/shell/src/functions.py "admin_goodbye"
 
-  Have some feedback?, please contact us at <$BENDER_EMAIL_DEVELOP>,
-  Or just contact $BENDER_SYSTEM_ADMIN.
-
-  kindly,
-  - bender system admin -
-
-EOF
 }
 
 _bender_check_if_bash ()
 {
-    if [ "$CATKIN_SHELL" = "bash" ]; then
-        return 0
-    fi
-    return 1
+    python "$BENDER_SYSTEM"/shell/src/functions.py "shell_is_bash"
 }
 
 _bender_check_if_zsh ()
 {
-    if [ "$CATKIN_SHELL" = "zsh" ]; then
-        return 0
-    fi
-    return 1
+    python "$BENDER_SYSTEM"/shell/src/functions.py "shell_is_zsh"
 }
 
 _bender_check_if_bash_or_zsh ()
 {
-    if _bender_check_if_bash || _bender_check_if_zsh ; then
+    python "$BENDER_SYSTEM"/shell/src/functions.py "shell_is_bash_or_zsh"
+}
+
+_bender_check_var_isset ()
+{
+    python "$BENDER_SYSTEM"/shell/src/functions.py "check_var_isset" "$1"
+}
+
+_bender_check_file_ext ()
+{
+	python "$BENDER_SYSTEM"/shell/src/functions.py "check_file_ext" "$1" "$2"
+}
+
+_bender_get_filepath ()
+{
+	python "$BENDER_SYSTEM"/shell/src/functions.py "get_filepath" "$1"
+}
+
+_bender_check_user_confirmation ()
+{
+    local answer
+    printf "Are you sure you want to do this? [y/N]: "
+    read answer
+    if echo "$answer" | grep -iq "^y" ;then
         return 0
+    else
+        return 1
     fi
-    return 1
+}
+
+_bender_get_active_ip ()
+{
+    ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | sed 's/\/.*//'
 }
 
 _bender_check_installed ()
 {
-    ## this only works on bash!!.
+    ## this only works on bash and zsh!!.
     ## 
     if [ -z "$1" ]; then
         echo "_bender_check_installed requires 1 argument: <executable_name>"
@@ -60,7 +76,7 @@ _bender_check_installed ()
 
 _bender_check_installed_and_exit ()
 {
-    ## this only works on bash!!.
+    ## this only works on bash and zsh!!.
     ## 
     if [ -z "$1" ]; then
         echo "_bender_check_installed_and_exit requires 1 argument: <executable_name>"
@@ -75,143 +91,3 @@ _bender_check_installed_and_exit ()
     fi
     return 0
 }
-
-# returns 1 if var is unset
-# returns 0 otherwise
-_bender_check_var_isset ()
-{
-    if [ -z "$1" ]; then
-        echo "_bender_check_var_isset requires 1 argument: <var_name>"
-        _bender_admin_goodbye
-        exit 1
-    fi
-
-    local varname="$1"
-    if [ -z "${!varname+x}" ]; then
-        #echo "var '$varname' is unset"
-        return 1
-    else
-        #echo "var is set to ${!varname}"
-        return 0
-    fi
-}
-
-# 
-# _bender_check_file_ext <filename> <ext> 
-# - returns 0: if <ext> is the file extension of <filename> 
-# - returns 1: otherwise
-_bender_check_file_ext ()
-{
-    if [ -z "$1" ]; then
-        echo "_bender_check_file_ext requires 2 arguments: <filename> <ext>"
-        _bender_admin_goodbye
-        exit 1
-    fi
-    if [ -z "$2" ]; then
-        echo "_bender_check_file_ext requires 2 arguments: <filename> <ext>"
-        _bender_admin_goodbye
-        exit 1
-    fi
-
-    local _filename="$1"
-    local _fileext="$2"
-
-    local _basename=${_filename##*/}
-    local _name=${_basename%.*}
-    local _ext=${_basename##*.}
-    local _basepath=${_filename%/*}
-
-    # no extension!
-    if [ "$_basename" = "$_ext" ]; then 
-        _ext="" 
-    fi
-
-    # echo "file: $_jsgf_file"
-    # echo "basename: ${_basename}"
-    # echo "basepath: ${_basepath}"
-    # echo "name: ${_name}"
-    # echo "ext : ${_ext}"
-    # echo ""
-
-    # check
-    if [ "$_fileext" = "$_ext" ]; then
-        return 0
-    else
-        echo "File extension not supported for file: $_basename"
-        echo " - found: '.$_ext'."
-        echo " - must be: '.$_fileext'."
-        return 1
-    fi
-}
-
-
-# 
-# _bender_get_filepath <filename> 
-# - writes the path to stdout.
-# - example: filename=/usr/bin/foo path=/usr/bin
-# - example: filename=../bin/foo path=../bin
-# - example: filename=foo path=.
-_bender_get_filepath ()
-{
-    if [ -z "$1" ]; then
-        echo "_bender_get_filepath requires 1 argument: <filename>"
-        _bender_admin_goodbye
-        exit 1
-    fi
-    local _filename="$1"
-    local _basepath=${_filename%/*}
-
-    if [ "$_basepath" = "$_filename" ]; then
-        _basepath="."
-    fi
-
-    # response
-    echo "$_basepath"
-}
-
-# 
-# _bender_get_file_noextname <filename> 
-# - writes the name to stdout.
-# - example: filename=/usr/bin/foo   name=foo
-# - example: filename=../bin/foo.bar name=foo
-# - example: filename=foo            name=foo
-# - example: filename=foo.tar.gz     name=foo
-_bender_get_file_noextname ()
-{
-    if [ -z "$1" ]; then
-        echo "_bender_get_filebasename requires 1 argument: <filename>"
-        _bender_admin_goodbye
-        exit 1
-    fi
-    local _filename="$1"
-    local _basename=${_filename##*/}
-    local _name=${_basename%%.*}
-
-    # response
-    echo "$_name"
-}
-
-
-_bender_check_user_confirmation ()
-{
-    local answer
-    printf "Are you sure you want to do this? [y/N]: "
-    read answer
-    if echo "$answer" | grep -iq "^y" ;then
-        return 0
-    else
-        return 1
-    fi
-}
-
-_bender_get_active_ip ()
-{
-    ip addr | grep 'state UP' -A2 | tail -n1 | awk '{print $2}' | sed 's/\/.*//'
-}
-
-
-# # tests
-# _bender_get_file_noextname "/usr/bin/foo"
-# _bender_get_file_noextname "../bin/foo.bar"
-# _bender_get_file_noextname "foo"
-# _bender_get_file_noextname "foo.tar.gz"
