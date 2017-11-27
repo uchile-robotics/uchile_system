9a8f93c20590a337ef298847ba5eba17bce3ed8e
bender <uchilehomebreakers@gmail.com>
2017-11-27 14:18:07 -0300

    fix error with bender red ip

--- install/nfs/uchile_nfs_mount.bash
+++ install/nfs/uchile_nfs_mount.bash
@@ -5,7 +5,7 @@
 #TODO: Documentar instalador.
 #TODO: Crear una nueva variable de ambiente que guarde la ip del nfs.
 
-if grep -Fxq "$UCHILE_NET_IP_BENDER_RED:/home/bender/uchile_ws /home/bender/uchile_ws nfs auto,nofail,noatime,nolock,intr,tcp,actimeo=1800 0 0" /etc/fstab
+if grep -Fxq "$UCHILE_NFS_IP_MASTER:/home/bender/uchile_ws /home/bender/uchile_ws nfs auto,nofail,noatime,nolock,intr,tcp,actimeo=1800 0 0" /etc/fstab
 then
     printf " #######################################\n"
     printf " ALL SET \n"
@@ -14,9 +14,9 @@ else
     printf " #######################################\n"
     printf " NFS MASTER IP NOT SET IN THE FSTAB \n"
     printf " #######################################\n"
-    printf " SETTING IP" "$UCHILE_NET_IP_BENDER_RED" "\n"
+    printf " SETTING IP " "$UCHILE_NFS_IP_MASTER" "\n"
     sudo printf " \n" | sudo tee --append /etc/fstab > /dev/null
-    sudo echo "$UCHILE_NET_IP_BENDER_RED:/home/bender/uchile_ws /home/bender/uchile_ws nfs auto,nofail,noatime,nolock,intr,tcp,actimeo=1800 0 0" | sudo tee --append /etc/fstab > /dev/null
+    sudo echo "$UCHILE_NFS_IP_MASTER:/home/bender/uchile_ws /home/bender/uchile_ws nfs auto,nofail,noatime,nolock,intr,tcp,actimeo=1800 0 0" | sudo tee --append /etc/fstab > /dev/null
     printf " ALL SET \n"
     printf " PLEASE RESTART TO SEE THE CHANGES \n"
 fi
