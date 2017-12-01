f71316b0f76cf2d823fe277aa4a38ebfa78b23c6
bender <uchilehomebreakers@gmail.com>
2017-12-01 11:59:42 -0300

    make a changes via ssh in the client computer to make the nfs mount automatic when the pc boot

--- install/nfs/uchile_nfs_mount.bash
+++ install/nfs/uchile_nfs_mount.bash
@@ -4,8 +4,9 @@
 #TODO: hacer un metodo para que quede mas simple esto.
 #TODO: Documentar instalador.
 #TODO: Crear una nueva variable de ambiente que guarde la ip del nfs.
+#TODO: que pruebe conecciones trate de modificarlo
 
-if grep -Fxq "$UCHILE_NFS_IP_MASTER:/home/bender/uchile_ws /home/bender/uchile_ws nfs auto,nofail,noatime,nolock,intr,tcp,actimeo=1800 0 0" /etc/fstab
+if ssh green -t "grep -Fxq '$UCHILE_NET_IP_BENDER_RED:/home/bender/uchile_ws /home/bender/uchile_ws nfs auto,nofail,noatime,nolock,intr,tcp,actimeo=1800 0 0' /etc/fstab"
 then
     printf " #######################################\n"
     printf " ALL SET \n"
@@ -14,9 +15,9 @@ else
     printf " #######################################\n"
     printf " NFS MASTER IP NOT SET IN THE FSTAB \n"
     printf " #######################################\n"
-    printf " SETTING IP " "$UCHILE_NFS_IP_MASTER" "\n"
-    sudo printf " \n" | sudo tee --append /etc/fstab > /dev/null
-    sudo echo "$UCHILE_NFS_IP_MASTER:/home/bender/uchile_ws /home/bender/uchile_ws nfs auto,nofail,noatime,nolock,intr,tcp,actimeo=1800 0 0" | sudo tee --append /etc/fstab > /dev/null
+    printf " SETTING IP " "$UCHILE_NET_IP_BENDER_RED" "\n"
+    ssh green -t "printf ' \n' | sudo tee --append /etc/fstab > /dev/null"
+    ssh green -t "echo '$UCHILE_NET_IP_BENDER_RED:/home/bender/uchile_ws /home/bender/uchile_ws nfs auto,nofail,noatime,nolock,intr,tcp,actimeo=1800 0 0' | sudo tee --append /etc/fstab > /dev/null"
     printf " ALL SET \n"
     printf " PLEASE RESTART TO SEE THE CHANGES \n"
 fi
